984587fc42bb857517b5688819e3a9898769d45a
wyao <willjarvin@gmail.com>
2023-07-22 17:09:57 +0800

    feat: init the project and add basic controller, templates

---
+++ src/main/java/com/github/willyao0523/springboot3inpractice/SprinigBoot3InPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.github.willyao0523.springboot3inpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinigBoot3InPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinigBoot3InPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/willyao0523/springboot3inpractice/SprinigBoot3InPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.willyao0523.springboot3inpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinigBoot3InPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
